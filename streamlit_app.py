b058cc50bab3f7d649efa6b1b716cfba9efbc014
ppizzi <43932233+ppizzi@users.noreply.github.com>
2022-06-17 11:52:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import requests
+import pandas as pd
 
 
 def fetch(session, url):
@@ -9,6 +10,13 @@ def fetch(session, url):
     except Exception:
         return {}
 
+    
+def fetchdf(session, url):
+    try:
+        result = session.get(url)
+        return result
+    except Exception:
+        return {}
 
 def main():
     st.set_page_config(page_title="Example App", page_icon="🤖")
@@ -22,11 +30,16 @@ def main():
         if submitted:
             st.write("Result")
             data = fetch(session, f"https://picsum.photos/id/{index}/info")
+            url1 = 'https://geoportalgasolineras.es/geoportalmovil/eess/search.do?tipoCarburante=4&rotulo=&venta=P&provincia=13&localidad=7339&tipoDestinatarioPlan=&operador=&nombrePlan=&calle=&numero=&codPostal='
+            data1 = fetchdf(session1, url1)
             if data:
                 st.image(data['download_url'], caption=f"Author: {data['author']}")
             else:
                 st.error("Error")
 
+            if data1:
+                df=data[0]
+                df
 
 if __name__ == '__main__':
     main()
