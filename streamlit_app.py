744a7c41a3de0e364e38d99d2b3be14073cc1687
ppizzi <43932233+ppizzi@users.noreply.github.com>
2022-06-18 15:28:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ def update_hist(df, hist_df):
 def plot_hist(hist_df):
     hist_df.Precio = hist_df.astype(float)
     hist_df.date = hist_df.astype(datetime)
-    st.line_chart(hist_df.loc[:, ['date','Precio'] ]) 
+    st.line_chart(hist_df.loc[hist_df['Rótulo'] == 'FAST FUEL', ['date','Precio'] ]) 
     
     return
 
