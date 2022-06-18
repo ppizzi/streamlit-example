911d8a1db9570a29187fb6c35ef64215d82fb253
ppizzi <43932233+ppizzi@users.noreply.github.com>
2022-06-18 15:35:12 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,8 @@ def update_hist(df, hist_df):
 
 def plot_hist(hist_df):
     hist_df.Precio = hist_df.Precio.astype(float)
-    hist_df.date = hist_df.date.astype(datetime)
+    #hist_df.date = hist_df.date.astype(datetime)
+    hist_df.date = pd.to_datetime(hist_df.date,format='%Y%m%d')
     st.write('dtypes new df with date: ', df.dtypes.astype(str))
     st.line_chart(hist_df.loc[hist_df['Rótulo'] == 'FAST FUEL', ['date','Precio'] ]) 
     
