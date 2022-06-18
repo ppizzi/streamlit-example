d7a3f08230e631f879545f2d3d05d26800fb837d
ppizzi <43932233+ppizzi@users.noreply.github.com>
2022-06-18 09:55:36 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,48 +51,12 @@ def main():
                 
                 try: 
                     hist_df = pd.read_csv('gasolineras_ciudad_real.csv') 
-                    hist_df.shape
+                    st.write(hist_df.shape)
                     #hist_df.head(5)
                     hist_df = hist_df.append(df, ignore_index=True)
-                    st.text('shape: ', hist_df.shape)
-                    st.dataframe( hist_df.tail(5) )
-                    hist_df.sort_values(by=['Rótulo', 'Dirección','date'], ascending=[True, True, True], ignore_index=True, inplace=True)
-                    hist_df.drop_duplicates(subset=['Rótulo','Dirección','date'], keep='first', inplace=True, ignore_index=True)
-                    st.text('shape: ', hist_df.shape)
-                    #hist_df.head(20)
-                    hist_df.to_csv('gasolineras_ciudad_real.csv', index=False)
-                    gasolineras = list(hist_df.Dirección.unique() )
-                    gasolineras
-                    gasolineras_ID = ['A','B','C','D','E','F','G','H','I','J','L','M','N','O','P']
-                    if not(len(gasolineras) == len(gasolineras_ID)) :
-                        input('there is a mismatch between the two lists')
-                    else:
-                        gas_df = pd.DataFrame( {'gasolineras': gasolineras, 'ID':gasolineras_ID})
-                        gas_df
-                    
-                    hist_df['ID'] = list(map(lambda x: gasolineras_ID[gasolineras.index(x)], hist_df['Dirección'] ) )
-                    hist_df
-                    hist_df.date = pd.to_datetime(hist_df.date,format='%Y%m%d')
-                    hist_df.head(5)
-                    hist_df['address']=hist_df.Rótulo + ' ' + hist_df.Dirección
-                    hist_df
-                    hist_df_hor = hist_df.pivot(index = 'date' , columns = ['ID'], values ='Precio')
-                    hist_df_hor.head(5)
-                    hist_df_hor.reset_index(inplace=True)
-                    print(hist_df_hor.dtypes)
-                    hist_df_hor.head()
-                    legenda = hist_df[['ID', 'Rótulo', 'Dirección']].drop_duplicates(subset='ID')
-                    legenda
-                    
-                    fig, ax = plt.subplots()
-                    ax.plot(hist_df_hor.date, hist_df_hor.H, color='green', marker='x', label='Fast Fuel')
-                    ax.plot(hist_df_hor.date, hist_df_hor.B, color='red', marker='x', label='Cepsa')
-                    plt.grid(True)
-                    ax.set_xlabel("Time")
-                    ax.set_ylabel("Price")
-                    fig.suptitle("Precio gasoleo Valdepenas")
-                    plt.legend(loc='upper left')
-                except:
+                    st.write(new shape: ', hist_df.shape)
+                    st.dataframe( hist_df.tail(5)
+                 except:
                     st.text('file not found')
 
                 
