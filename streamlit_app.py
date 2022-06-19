295a197e5b4d009c491d9dee6d8350e9eeab7f0e
ppizzi <43932233+ppizzi@users.noreply.github.com>
2022-06-19 19:47:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,7 @@ def run_query(query):
 
 
 def googlesheetsdf(sheet_url):
-
+    sheet_url = st.secrets["public_gsheets_url"]
     rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
     # Print results.
@@ -120,7 +120,6 @@ def main():
         # Perform SQL query on the Google Sheet.
         # Uses st.cache to only rerun when the query changes or after 10 min.
         @st.cache(ttl=600)
-        sheet_url = st.secrets["public_gsheets_url"]
         googlesheetsdf(sheet_url)
         
                     
