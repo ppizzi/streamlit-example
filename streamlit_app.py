05d03c7c54a51af9ef3da3256e0f1fe7862af3f7
ppizzi <43932233+ppizzi@users.noreply.github.com>
2022-06-19 19:27:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,26 @@ import streamlit as st
 import streamlit.components.v1 as components
 import requests
 import pandas as pd
+from gsheetsdb import connect
 
-#import matplotlib.pyplot as plt
 
+# Create a connection object.
+conn = connect()
+
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+
+sheet_url = st.secrets["public_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+# Print results.
+for row in rows:
+    st.write(f"{row.name} has a :{row.pet}:")
 
 
 #USEFUL LINKS
