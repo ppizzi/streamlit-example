480ca993e34d47c626a40ca67b2e57306d2819b0
ppizzi <43932233+ppizzi@users.noreply.github.com>
2022-06-25 12:29:50 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -151,7 +151,8 @@ def main():
         #FROM "https://docs.google.com/spreadsheets/d/1_rN3lm0R_bU3NemO0s9pbFkY5LQPcuy1pscv8ZXPtg8/edit#gid=1648320094"
         #FROM "sheet_url"
         for row in cursor.execute(SQL):
-            print(row)
+            #print(row)
+            st.write(row)
    
         #-----------------  #shillelagh
     
