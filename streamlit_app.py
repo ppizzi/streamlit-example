ecc32a796dc28cb205c06f77386627930c1112a7
ppizzi <43932233+ppizzi@users.noreply.github.com>
2022-06-25 14:08:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,15 +38,6 @@ st.title("Get gas prices in Valdepeñas")
 @st.cache(suppress_st_warning=True)                #<-works
 
 
-#---------- GOOGLE CLOUD
-# Retrieve file contents.
-# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-@st.experimental_memo(ttl=600)
-def read_file(bucket_name, file_path):
-    bucket = client.bucket(bucket_name)
-    content = bucket.blob(file_path).download_as_string().decode("utf-8")
-    return content
-#----------
 
 
 
@@ -144,6 +135,16 @@ def main():
         
         
         #------------GOOGLE OATUH code
+        #---------- GOOGLE CLOUD
+        # Retrieve file contents.
+        # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+        @st.experimental_memo(ttl=600)
+        def read_file(bucket_name, file_path):
+            bucket = client.bucket(bucket_name)
+            content = bucket.blob(file_path).download_as_string().decode("utf-8")
+            return content
+        #----------
+    
         # Create API client.
         credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
         client = storage.Client(credentials=credentials)
